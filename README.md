dcd4801021a4699a140153cad91bbbb7a902a021
William Huebner <wilhueb@gmail.com>
2025-04-09 22:45:32 -0400

    finish chalamet

---
+++ README.md
@@ -0,0 +1,6 @@
+### Chalamet sequence
+
+Closest example I could find is Darvish to Moncada, 2020-09-25:
+1. https://baseballsavant.mlb.com/sporty-videos?playId=4014c0d3-0ce2-4b43-bd6f-4e9586a26fab
+2. https://baseballsavant.mlb.com/sporty-videos?playId=0af21e8a-bff1-4109-bb47-090cd3688bb8
+3. https://baseballsavant.mlb.com/sporty-videos?playId=5dec452a-45e2-40af-b047-f563c9259bea
--- chalamet-sequence.py
+++ chalamet-sequence.py
@@ -46,7 +46,8 @@ def pickle_dump(data: Any, path: pathlib.Path | str):
     pathlib.Path(f.name).rename(path)
 
 
-plays = []
+best_plays = []
+best_score = 0
 
 schedules = {}
 
@@ -56,31 +57,33 @@ try:
 except FileNotFoundError:
     pass
 
-for year in range(2025, 2000, -1):
+for year in range(2025, 2007, -1):
     games = schedules.get(year, None)
     if games is None:
-        print(f"fetching schedule for {year} season")
+        print(f"fetching {year} games")
         games = get_games(year)
 
         if year < datetime.datetime.now().year:
             schedules[year] = games
-            print(f"writing schedule for {year} season")
-            pickle_dump(schedules, "pickles/schedules.pickle")
+            file = "pickles/schedules.pickle"
+            print(f"writing {file}")
+            pickle_dump(schedules, file)
 
     play_by_play = {}
 
     try:
-        with open(f"pickles/pbp{year}.pickle", "rb") as f:
-            print(f"reading play by play for {year} season")
+        file = f"pickles/pbp{year}.pickle"
+        with open(file, "rb") as f:
+            print(f"reading {file}")
             play_by_play = pickle.load(f)
     except FileNotFoundError:
         pass
 
-    count = 0
-    count_bad = 0
-
     for scheduled in tqdm(games, desc=f"{year} season"):
         if scheduled["status"] != "Final":  # hasn't been played yet
+            continue
+
+        if scheduled["game_type"] in ["E", "S"]:  # exhibition or spring training
             continue
 
         game_id = scheduled["game_id"]
@@ -102,63 +105,76 @@ for year in range(2025, 2000, -1):
 
             pitches = [play["playEvents"][i] for i in pitch_idxs]
 
-            first, second, third = pitches
+            # automatic strike
+            if any(pitch["details"]["code"] == "AC" for pitch in pitches):
+                continue
 
-            count += 1
+            first, second, third = pitches
 
             try:
                 # counting sweeper as a slider
                 if first["details"]["type"]["code"] not in ["SL", "ST"]:
                     continue
 
-                bat_side = play["matchup"]["batSide"]["code"]
-
-                outside = CATCHER_RIGHT if bat_side == "R" else CATCHER_LEFT
-
-                def get_zone(pitch):
-                    if "zone" in pitch["pitchData"]:
-                        return pitch["pitchData"]["zone"]
-                    return pitch["details"]["zone"]
-
-                # not outside
-                if get_zone(first) not in outside:
-                    continue
-
                 # counting knuckle curve as a curveball
                 if second["details"]["type"]["code"] not in ["CU", "KC"]:
-                    continue
-
-                # not a swinging strike
-                if second["details"]["code"] != "S":
-                    continue
-
-                # not below the zone
-                if get_zone(second) not in [13, 14]:
                     continue
 
                 # counting sinker as a fastball
                 if third["details"]["type"]["code"] not in ["FF", "SI"]:
                     continue
-
-                # not a called strike
-                if third["details"]["code"] != "C":
-                    continue
-
-                inside = CATCHER_LEFT if bat_side == "R" else CATCHER_RIGHT
-
-                # not inside
-                if get_zone(third) not in inside:
-                    continue
-            except Exception:  # pitch data missing
-                count_bad += 1
+            except KeyError:
                 continue
 
-            pprint(play)
-            plays.append(play)
+            def get_score(play):
+                score = 0
 
-    print(f"{count_bad / count:.2%} of plays missing data")
-    print(f"writing play by play for {year} season")
-    pickle_dump(play_by_play, f"pickles/pbp{year}.pickle")
+                bat_side = play["matchup"]["batSide"]["code"]
+                outside = CATCHER_RIGHT if bat_side == "R" else CATCHER_LEFT
+                inside = CATCHER_LEFT if bat_side == "R" else CATCHER_RIGHT
 
-with open("plays.json", "w") as f:
-    json.dump(plays, f, indent=4)
+                # outside
+                if first["pitchData"]["zone"] in outside:
+                    score += 1
+
+                # dirt
+                if second["pitchData"]["zone"] in [13, 14]:
+                    score += 1
+
+                # chase
+                if second["details"]["code"] == "S":
+                    score += 1
+
+                # 97+ mph
+                if third["pitchData"]["startSpeed"] >= 96.5:
+                    score += 1
+
+                # inside
+                if third["pitchData"]["zone"] in inside:
+                    score += 1
+
+                # called strike
+                if third["details"]["code"] == "C":
+                    score += 1
+
+                return score
+
+            score = get_score(play)
+            if score >= best_score:
+                if score > best_score:
+                    best_plays.clear()
+                best_plays.append(play)
+                best_score = score
+
+    best_plays.sort(key=lambda play: play["about"]["startTime"])
+    print(f"{best_score=}")
+    print(
+        "\n".join(
+            f"{play['about']['startTime']} - {play['matchup']['pitcher']['fullName']} to {play['matchup']['batter']['fullName']}"
+            for play in best_plays
+        )
+    )
+
+    file = f"pickles/pbp{year}.pickle"
+    print(f"writing {file}")
+    pickle_dump(play_by_play, file)
